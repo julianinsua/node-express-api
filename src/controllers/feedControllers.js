2b3751bb8590fff6ed7f4e4e62d72ff505b4e1c0
Julian <jii.julious@gmail.com>
2021-09-19 22:23:53 -0300

    Module 25 - Class 13: Uploading Images

--- src/controllers/feedControllers.js
+++ src/controllers/feedControllers.js
@@ -1,11 +1,77 @@
+const { validationResult } = require("express-validator");
+const Post = require("../models/Post");
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({posts:[{title: 'First Post', content: 'This is the firstPost'}]})
-}
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({ message: "fetched posts successfully.", posts });
+    })
+    .catch((e) => {
+      if (!e.statusCode) {
+        e.statusCode = 500;
+      }
+      next(e);
+    });
+};
 
 exports.createPost = (req, res, next) => {
-    const {title, content} = req.body
+  const errors = validationResult(req);
 
-    // Create a post in the database
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed");
+    error.status = 422;
+    throw error;
+  }
 
-    return res.status(201).json({message: 'post created successfully', post: {id: new Date().toISOString(), title, content}})
-}
+  if (!req.file) {
+    const error = new Error("No image provided.");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const imageUrl = req.file.path;
+  const { title, content } = req.body;
+
+  const post = new Post({
+    title,
+    content,
+    imageUrl,
+    creator: { name: "pepito" },
+  });
+
+  post
+    .save()
+    .then((result) => {
+      return res.status(201).json({
+        message: "post created successfully",
+        post: result,
+      });
+    })
+    .catch((e) => {
+      if (!e.statusCode) {
+        e.statusCode = 500;
+      }
+      next(e);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const { postId } = req.params;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Couldn't find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      res.status(200).json({ message: "Post fetched", post });
+    })
+    .catch((e) => {
+      if (!e.statusCode) {
+        e.statusCode = 500;
+      }
+      next(e);
+    });
+};
--- src/routes/feedRoutes.js
+++ src/routes/feedRoutes.js
@@ -1,12 +1,26 @@
-const {Router} = require('express')
-const {getPosts, createPost} = require('../controllers/feedControllers')
+const { Router } = require("express");
+const { body } = require("express-validator");
+const {
+  getPosts,
+  createPost,
+  getPost,
+} = require("../controllers/feedControllers");
 
-const router = Router()
+const router = Router();
 
 //GET => /feed/posts
-router.get('/posts', getPosts)
+router.get("/posts", getPosts);
 
 //POST => /feed/posts
-router.post('/posts', createPost)
+router.post(
+  "/posts",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  createPost
+);
 
-module.exports = router
+router.get("/post/:postId", getPost);
+
+module.exports = router;
